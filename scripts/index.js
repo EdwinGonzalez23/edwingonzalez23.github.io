c88291f84e6b574b3a8df3d69d5eba897a256b46
Edwin Gonzalez <gonzalez.edwin2311@gmail.com>
2019-12-23 16:35:33 -0800

    added index.html with dynamic pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,12 @@
-$("#index").load("b.html"); 
 $(document).ready(function () {
     //By Default Load Home
-   // $(function(){
-        $("#index").html("sadfsdafsa");
-      //});
-    $("#projects").click(function (e) { 
-        $(".home-page").hide();
-        $(".projects").show();
-    });
+    $("#index").load("includes/home.html"); 
     $("#home").click(function (e) { 
+        $("#index").load("includes/home.html");
         $(".prjects").hide();
         $(".home-page").show();
+    });
+    $("#projects").click(function (e) { 
+        $("#index").load("includes/projects.html");
     });
 });
